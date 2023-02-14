c5ea827a0bc07a9d2bd000f914278be859192c03
Linuss <tade3d@gmail.com>
2023-02-14 15:38:52 +0100

    Created function to get all photos of logged in user.

--- src/controllers/photo_controller.ts
+++ src/controllers/photo_controller.ts
@@ -5,6 +5,7 @@ import Debug from "debug";
 import { Request, Response } from "express";
 import { validationResult } from "express-validator";
 import prisma from "../prisma";
+import { getUserByEmail } from "../services/user_service";
 
 // Create a new debug instance
 const debug = Debug("prisma-photos:photo_controller");
@@ -12,7 +13,30 @@ const debug = Debug("prisma-photos:photo_controller");
 /**
  * Get all photos
  */
-export const index = async (req: Request, res: Response) => {};
+export const index = async (req: Request, res: Response) => {
+  // const profile = getUserByEmail(req.token!.email);
+  console.log(req.token);
+
+  try {
+    const photos = await prisma.photo.findMany({
+      where: {
+        userId: req.token!.sub,
+      },
+    });
+
+    res.send({
+      status: "success",
+      data: photos,
+    });
+  } catch (err) {
+    debug("Error thrown when finding photos", err);
+
+    res.status(500).send({
+      status: "error",
+      message: "Something went wrong",
+    });
+  }
+};
 
 /**
  * Get a single photo
