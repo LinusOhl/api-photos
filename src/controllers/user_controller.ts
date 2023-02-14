4c3bc339732e95dbac57ec5275e2c69ed5390e8b
Linuss <tade3d@gmail.com>
2023-02-14 14:39:47 +0100

    Created login & refresh function for User.

--- src/controllers/user_controller.ts
+++ src/controllers/user_controller.ts
@@ -44,8 +44,8 @@ export const register = async (req: Request, res: Response) => {
   // Store the user in the database
   try {
     const user = await createUser({
-      first_name: validatedData.first_name,
-      last_name: validatedData.last_name,
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
       email: validatedData.email,
       password: validatedData.password,
     });
@@ -63,7 +63,68 @@ export const register = async (req: Request, res: Response) => {
 /**
  * Login a user
  */
-export const login = async (req: Request, res: Response) => {};
+export const login = async (req: Request, res: Response) => {
+  // destructure email and password from request body
+  const { email, password } = req.body;
+
+  // find user with email, otherwise bail
+  const user = await getUserByEmail(email);
+  if (!user) {
+    return res.status(401).send({
+      status: "fail",
+      message: "Authorization required",
+    });
+  }
+
+  // verify credentials against hash, otherwise bail
+  const result = await bcrypt.compare(password, user.password);
+  if (!result) {
+    return res.status(401).send({
+      status: "fail",
+      message: "Authorization required",
+    });
+  }
+
+  // construct jwt-payload
+  const payload: JwtPayload = {
+    sub: user.id,
+    email: user.email,
+    first_name: user.first_name,
+    last_name: user.last_name,
+  };
+
+  // sign payload with access-token secret and get access-token
+  if (!process.env.ACCESS_TOKEN_SECRET) {
+    return res.status(500).send({
+      status: "error",
+      message: "No access-token secret defined",
+    });
+  }
+
+  const access_token = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: process.env.ACCESS_TOKEN_LIFETIME || "4h",
+  });
+
+  // sign payload with refresh-token secret and get refresh-token
+  if (!process.env.REFRESH_TOKEN_SECRET) {
+    return res.status(500).send({
+      status: "error",
+      message: "No refresh-token secret defined",
+    });
+  }
+
+  const refresh_token = jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: process.env.REFRESH_TOKEN_LIFETIME || "1d",
+  });
+
+  res.send({
+    status: "success",
+    data: {
+      access_token,
+      refresh_token,
+    },
+  });
+};
 
 /**
  * Refresh token
@@ -72,4 +133,60 @@ export const login = async (req: Request, res: Response) => {};
  *
  * Authorization: Bearer <refresh-token>
  */
-export const refresh = async (req: Request, res: Response) => {};
+export const refresh = async (req: Request, res: Response) => {
+  if (!req.headers.authorization) {
+    debug("Authorization header missing!");
+
+    return res.status(401).send({
+      status: "fail",
+      data: "Authorization missing",
+    });
+  }
+
+  const [authSchema, token] = req.headers.authorization.split(" ");
+
+  if (authSchema.toLocaleLowerCase() !== "bearer") {
+    debug("Authorization schema isn't bearer!");
+
+    return res.status(401).send({
+      status: "fail",
+      data: "Authorization required",
+    });
+  }
+
+  try {
+    const payload = jwt.verify(
+      token,
+      process.env.REFRESH_TOKEN_SECRET || ""
+    ) as unknown as JwtPayload;
+
+    delete payload.iat;
+    delete payload.exp;
+
+    // sign payload with access-token secret and get access-token
+    if (!process.env.ACCESS_TOKEN_SECRET) {
+      return res.status(500).send({
+        status: "error",
+        message: "No access-token secret defined",
+      });
+    }
+
+    const access_token = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: process.env.ACCESS_TOKEN_LIFETIME || "4h",
+    });
+
+    res.send({
+      status: "success",
+      data: {
+        access_token,
+      },
+    });
+  } catch (err) {
+    debug("Token failed verification", err);
+
+    return res.status(401).send({
+      status: "fail",
+      data: "Authorization required",
+    });
+  }
+};
