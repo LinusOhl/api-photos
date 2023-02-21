105fee9bc424874d6508a58d58e7743457861fd2
Linuss <tade3d@gmail.com>
2023-02-21 08:36:17 +0100

    Created validation checks for when creating/updating a photo.

---
+++ src/validations/photo_rules.ts
@@ -0,0 +1,32 @@
+/**
+ * Validation Rules for Photo resource
+ */
+import { body } from "express-validator";
+import { getUserByEmail } from "../services/user_service";
+
+export const createPhotoRules = [
+  body("title")
+    .isString()
+    .isLength({ min: 3 })
+    .withMessage("Has to be a string and at least 3 chars long."),
+  body("url").isURL().withMessage("Has to be a valid url."),
+  body("comment")
+    .optional()
+    .isString()
+    .isLength({ min: 3 })
+    .withMessage("Has to be a string and at least 3 chars long."),
+];
+
+export const updatePhotoRules = [
+  body("title")
+    .optional()
+    .isString()
+    .isLength({ min: 3 })
+    .withMessage("Has to be a string and at least 3 chars long."),
+  body("url").optional().isURL().withMessage("Has to be a valid url."),
+  body("comment")
+    .optional()
+    .isString()
+    .isLength({ min: 3 })
+    .withMessage("Has to be a string and at least 3 chars long."),
+];
